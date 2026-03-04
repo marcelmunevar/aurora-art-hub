ceaf4b6d69a4a9befcbc503e8dcda960fa692fbe
Marcel Munevar <marcelmunevar@gmail.com>
2026-03-04 18:35:38 -0500

    moved header and footer to primary layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import { AuthButton } from "@/components/auth-button";
+import Link from "next/link";
+import { Suspense } from "react";
+import { hasEnvVars } from "@/lib/utils";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -33,7 +39,42 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main className="min-h-screen flex flex-col items-center">
+            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                  <div className="flex gap-5 items-center font-semibold">
+                    <Link href={"/"}>Aurora Art Hub</Link>
+                    <div className="flex items-center gap-2"></div>
+                  </div>
+                  {!hasEnvVars ? (
+                    <EnvVarWarning />
+                  ) : (
+                    <Suspense>
+                      <AuthButton />
+                    </Suspense>
+                  )}
+                </div>
+              </nav>
+              <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+                {children}
+              </div>
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+                <p>
+                  Powered by{" "}
+                  <a
+                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    target="_blank"
+                    className="font-bold hover:underline"
+                    rel="noreferrer"
+                  >
+                    Supabase
+                  </a>
+                </p>
+                <ThemeSwitcher />
+              </footer>
+            </div>
+          </main>
         </ThemeProvider>
       </body>
     </html>
