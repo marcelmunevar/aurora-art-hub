3b1d31a711fa0338c75265f401665eb4e65fe8d6
Marcel Munevar <marcelmunevar@gmail.com>
2026-03-05 17:58:10 -0500

    updated messaging

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">Just a paragraph</main>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        Welcome to Aurora Art Hub, a vibrant community where artists and art
+        enthusiasts come together to share, discover, and celebrate creativity.
+        Join us to explore a world of artistic expression and connect with
+        fellow art lovers!
+      </main>
     </>
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,10 @@
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <h1 className="sr-only">Aurora Art Hub</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with Supabase and Next.js
+        The best place to share and discover art. Join our vibrant community of
+        artists!
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
