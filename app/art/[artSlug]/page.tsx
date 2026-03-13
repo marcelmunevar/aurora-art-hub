1ea692be3aa3161fc3c97146da25699c9b1fbb58
Marcel Munevar <marcelmunevar@gmail.com>
2026-03-13 15:12:03 -0400

    removed dynamic routes for now

--- app/art/[artSlug]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>TODO: Implement art slug page</div>;
-}
--- app/tags/[tagSlug]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>TODO: Implement tag slug page</div>;
-}
